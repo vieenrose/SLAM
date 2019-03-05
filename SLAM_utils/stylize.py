e03e0982b82a8cc8a3c987018b84530be9a13272
vieenrose <luigi.liu@laposte.net>
2019-03-05 09:15:37 +0100

    adjust the position of target label and value

--- SLAM_utils/stylize.py
+++ SLAM_utils/stylize.py
@@ -429,14 +429,14 @@ def show_stylization(time_org,original,smooth,style1,style2,targetIntv,register,
     
     # target label
     txt = targetIntv.mark()
-    ann_target = ax.annotate(txt, xy=(.5*xticks_major[0]+.5*xticks_major[1],-0.13+.04-0.08-0.01),xycoords=('data','axes fraction'),fontsize=9,fontweight='medium',horizontalalignment='center',fontstyle='italic', bbox=bbox_props,ma='left')
+    ann_target = ax.annotate(txt, xy=(.5*xticks_major[0]+.5*xticks_major[1],-0.13+.04-0.08-0.01-0.04),xycoords=('data','axes fraction'),fontsize=9,fontweight='medium',horizontalalignment='center',fontstyle='italic', bbox=bbox_props,ma='left')
     wrap_text_to_contours_width(txt, ann_target, fig, lns2)
     
     if is_new_support:
           TargetLabel = 'Target'
           if targetName: TargetLabel+=' [{}]'.format(targetName)
           TargetLabel += ': '
-          ax.annotate(TargetLabel      ,xy=(labelsLeftPos,-0.13+.04-0.08-0.01)              ,xycoords=('axes fraction','axes fraction'),fontsize=11,fontweight='normal'  ,horizontalalignment='right')
+          ax.annotate(TargetLabel      ,xy=(labelsLeftPos,-0.13+.04-0.08-0.01-0.04)              ,xycoords=('axes fraction','axes fraction'),fontsize=11,fontweight='normal'  ,horizontalalignment='right')
           #ax.annotate('Global Labels: ',xy=(labelsLeftPos,-0.19+.04+.02-0.08-0.01-0.02-0.06),              xycoords=('axes fraction','axes fraction'),fontsize=11,fontweight='semibold',horizontalalignment='right',color=color_style_styl)
     
     # its stlization in symbolic form
